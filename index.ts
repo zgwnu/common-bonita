a766e955ac618d5b53861ce54b20d36b28646f5d
Onno Haldar <onno@zgw.nu>
2017-08-28 11:51:40 +0200

    refactor

--- index.ts
+++ index.ts
@@ -4,16 +4,17 @@ export * from './rest-api/zgwnu-bonita-search-parms'
 // Common Bonita Authentication
 export * from './authentication/zgwnu-bonita-credentials'
 
-// Common Bonita Bpm Process
-export * from './bpm-process/zgwnu-bonita-process-activation-state.type'
-export * from './bpm-process/zgwnu-bonita-process-configuration-state.type'
-export * from './bpm-process/zgwnu-bonita-create-case-response'
-export * from './bpm-process/zgwnu-bonita-deploy-process-definition-response'
-export * from './bpm-process/zgwnu-bonita-process-definition'
-export * from './bpm-process/zgwnu-bonita-process-update-input'
+// Common Bonita BPM
+// Common Bonita BPM Process
+export * from './bpm/process/zgwnu-bonita-process-activation-state.type'
+export * from './bpm/process/zgwnu-bonita-process-configuration-state.type'
+export * from './bpm/process/zgwnu-bonita-create-case-response'
+export * from './bpm/process/zgwnu-bonita-deploy-process-definition-response'
+export * from './bpm/process/zgwnu-bonita-process-definition'
+export * from './bpm/process/zgwnu-bonita-process-update-input'
 
-// Common Bonita Business Data
-export * from './business-data/zgwnu-bonita-business-data-query-parms'
+// Common Bonita BDM
+export * from './bdm/zgwnu-bonita-business-data-query-parms'
 
 // Common Bonita File Upload
 export * from './file-upload/zgwnu-bonita-file-upload-response'
