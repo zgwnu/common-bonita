a1fdb4fb8cd52827b78a73831deb6b359241286d
Onno Haldar <onno@zgw.nu>
2017-09-04 12:12:05 +0200

    refactor

---
+++ dist/bpm/process/zgwnu-bonita-deploy-process-definition-response.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ZgwnuBonitaDeployProcessDefinitionResponse = /** @class */ (function () {
+    function ZgwnuBonitaDeployProcessDefinitionResponse() {
+    }
+    return ZgwnuBonitaDeployProcessDefinitionResponse;
+}());
+exports.ZgwnuBonitaDeployProcessDefinitionResponse = ZgwnuBonitaDeployProcessDefinitionResponse;
